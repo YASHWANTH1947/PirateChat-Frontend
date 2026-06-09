de68cda7e3dcd5c8d04ca683f133b607ba0153fa
Yashwanth <yashwanthreddy1947@gmail.com>
2026-06-09 12:21:56 +0530

    the websockerr code updated

--- src/websockets/socket.js
+++ src/websockets/socket.js
@@ -4,8 +4,13 @@ const listeners = new Map(); // Store custom event handlers
 export const connectSocket = (token) => {
   if (socket) return; // Prevent multiple connections
 
-  socket = new WebSocket(`ws://localhost:8000?token=${token}`);
+  // socket = new WebSocket(`ws://localhost:8000?token=${token}`);
+  // Use the URL from your environment variables
+  const backendUrl = import.meta.env.VITE_API_BASE_URL.replace("http", "ws");
 
+  // If your base URL is https://piratechat.onrender.com/api
+  // This will correctly become wss://piratechat.onrender.com/api
+  socket = new WebSocket(`${backendUrl.replace("http", "ws")}/?token=${token}`);
   socket.addEventListener("open", () => {
     console.log("WebSocket connected");
   });
